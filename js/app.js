dcae05254c671bb7c7de0493e3fa8ddb44b2dcfc
Seinzu <git@andrewrumble.com>
2012-10-10 18:16:31 +0100

    Changing appearance of Game a little.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 (function ($){
     $(document).ready(function (){
-        $("#game-of-life").gameOfLife({updateInterval: 400, liveColour: '#2244FF', deadColour: '#FFFFFF'});
+        $("#game-of-life").gameOfLife({updateInterval: 400, liveColour: '#2244FF', deadColour: '#CCCCCC'});
     })
 })(jQuery);
